c686038ec516df8d1477d60cc9b559804cf0a261
Josef Ullrich? <josefullrich@icloud.com>
2021-01-22 03:24:02 +0000

    add project

---
+++ proj/encrypt/encrypt.c
@@ -0,0 +1,77 @@
+//
+// encrypt.c
+//
+
+#include <string.h>
+#include <stdio.h>
+#include "encrypt.h"
+#include <ctype.h>
+
+char CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int CHARS_LEN = 62;
+
+
+char shiftChar(char c, int shift, int direction)
+{
+  // implement the character shift here:
+  //  given a char c shift it either forwards (direction == 1) or backwards (direction == 0)
+  //  use the CHARS array above
+  //  return the newly shifted char
+  //
+  // eg.
+  //   shiftChar('c', 3, 1) : 'f'
+  //   shiftChar('S', 2, 0) : 'P'
+  //   shiftChar('b', 3, 0) : '8'
+  int index = c - 97;
+  if (direction == 1)
+  {
+    index = index + shift;
+  }
+  if (direction == 0)
+  {
+    index = index - shift;
+  }
+  if (index < 0)
+    index = index + 62;
+  if (index >= CHARS_LEN)
+    index = index - 62;
+  return CHARS[index];
+}
+
+
+void encrypt(char str[], int shifts[], int shiftslen)
+{
+  for (int i = 0; i < strlen(str); i++) 
+  {
+    char c = str[i];
+
+    str[i] = shiftChar(c, shifts[i%shiftslen], 1);
+  }
+}
+
+
+void decrypt(char str[], int shifts[], int shiftslen)
+{
+  for (int i = 0; i < strlen(str); i++) 
+  {
+    char c = str[i];
+
+    str[i] = shiftChar(c, shifts[i%shiftslen], 0);
+  }
+}
+
+
+void caesarEncrypt(char str[], int shift)
+{
+  int shifts[] = { shift };
+  encrypt(str, shifts, 1);
+}
+
+
+void caesarDecrypt(char str[], int shift)
+{
+  int shifts[] = { shift };
+  decrypt(str, shifts, 1);
+}
+
+
---
+++ proj/student.c
@@ -0,0 +1,145 @@
+//
+// student.c
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "student.h"
+#include "encrypt.h"
+
+
+const char* STUFILE = "studentdata.txt";
+
+// keeps track of the number of students in the students array
+int numStudents = 0;
+
+// holds the student pointer array
+Student* students[100] = {0};
+
+
+void createStudent(char* fname, char* lname, int age, int id)
+{
+  // createStudent dynamically creates a Student on the heap and adds that
+  // student to the student array
+  //  - the firstName and lastName strings should be dynamically created
+  //    based on the size of the fname and lname args
+  Student* st = (Student*) malloc(sizeof(Student));
+  st->firstName = (char*)malloc(strlen(fname)+1);
+  st->lastName = (char*)malloc(strlen(lname)+1);
+  strcpy(st->firstName, fname);
+  strcpy(st->lastName, lname);
+  st->age = age;
+  st->id = id;
+  students[numStudents] = st;
+  numStudents++;
+}
+
+
+void deleteStudent(Student* student)
+{
+  // free the memory associated with a student including the strings
+  free(student->firstName);
+  free(student->lastName);
+  free(student);
+}
+
+
+void deleteStudents()
+{
+  // iterate over the students array deleting every student and setting te pointer
+  // values to 0 and adjusting the numStudents to 0
+  for(int i = 0; i < numStudents; i++)
+  {
+	deleteStudent(students[i]);
+	students[i]=0;
+  }
+  numStudents=0;
+}
+
+char* FILE_NAME = "studentdata.txt";
+void saveStudents(int key)
+{
+  // save all students in the student array to a file 'studentdata.txt' overwriting
+  // any existing file
+  //   - the format of the file is one line per student as follows fname lname age id:
+  //       tom thumb 15 1234
+  //       james dean 21 2345
+  //       katy jones 18 4532
+  FILE* fp;
+  char buff[256];
+  fp = fopen("studentdata.txt", "w");
+  if (fp)
+  {
+    for (int i=0; i < numStudents; i++) {
+      sprintf(buff, "%s %s %d %ld", students[i]->firstName, students[i]->lastName, students[i]->age, students[i]->id);
+      if(key != 0)
+        caesarEncrypt(buff, key);
+      printf("saving: %s\n", buff);
+
+      fprintf(fp, "%s\n", buff);
+    }
+    fclose(fp);
+  }
+}
+
+
+void loadStudents(int key)
+{
+  if (numStudents > 0){
+    deleteStudents();
+  }
+
+
+  // load the students from the data file overwriting all exisiting students in memory
+  FILE* fp;
+  fp = fopen("studentdata.txt", "r");
+  if (fp)
+  {
+
+    while(1){
+      char b1[256]; char b2[256]; char b3[256]; char b4[256];
+      int match = fscanf(fp, "%s %s %s %s", b1, b2, b3, b4);
+      if(match == 4){
+        if (key != 0){
+          caesarDecrypt(b1, key);
+	  caesarDecrypt(b2, key);
+	  caesarDecrypt(b3, key);
+	  caesarDecrypt(b4, key);
+        }
+        int age;
+	long id;
+	sscanf(b3, "%d", &age);
+	sscanf(b4, "%ld", &id);
+	createStudent(b1, b2, age, id);
+      }
+      else
+        break;
+    }
+    printf("loaded %d students", numStudents);
+    fclose(fp);
+  }
+}
+
+
+void printStudent(Student* student)
+{
+  printf("  Student: %s %s\n", student->firstName, student->lastName);
+  printf("    age: %d\n", student->age);
+  printf("    id: %ld\n", student->id);
+}
+
+
+void printStudents()
+{
+  for (int i = 0; i < numStudents; i++) {
+    printf("\n----- student %d ------\n", i);
+    printStudent(students[i]);
+  }
+}
+
+
+
+
+
